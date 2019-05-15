46414f621aa146c2cda7ec8f607bd7b5014546fd
GeoHernandez27 <leopardo2@gmail.com>
2019-05-15 14:23:18 -0600

    styled header and navbar components

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -15,10 +15,12 @@ const INITIAL_STATE = {
     ],
     navbarLinks: [
         {
-            title: 'account'
+            title: 'Account',
+            active: true
         },
         {
-            title: 'purchases'
+            title: 'Purchases',
+            active: false
         }
     ] 
 

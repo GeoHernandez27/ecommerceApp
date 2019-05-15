dc0175e3344a32af84a59dc114d87c5077b70e2a
GeoHernandez27 <leopardo2@gmail.com>
2019-05-15 11:01:23 -0600

    styled details quick links component and reformatted layout

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -28,36 +28,27 @@ class SignInForm extends Component {
     ]
     return (
       <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
-        <Field
-          className="sign-in-form__email"
-          type="email"
-          title="Email"
-          placeholder="Email"
-          name="email"
-          component={FormInput}
-        />
-        <Field
-          className="sign-in-password"
-          type="password"
-          title="Password"
-          placeholder="Password"
-          name="password"
-          component={FormInput}
-        />
-        <div className="sign-in-form__line"></div>
-        <Field
-          className="sign-in-form__login"
-          onClick={() => console.log("Trying to submit")}
-          type="submit"
-          type="login"
-          title="Login"
-          name="login"
-          component={FormButton}
-        />
-        <Details className='sign-in-form__details' title='QuickLinks' links={links} />
-
-      </form>
-    );
+                <Field className='sign-in-form__email'
+                type='email'
+                title='Email'
+                placeholder='Email'
+                name='email'
+                component={FormInput}/>
+                <Field className='sign-in-form__password'
+                type='password'
+                title='Password'
+                placeholder='Password'
+                name='password'
+                component={FormInput}/>
+                <div className='sign-in-form__line'></div>
+                <Field className='sign-in-form__login'
+                type='submit'
+                title='Login'
+                name='login'
+                component={FormButton}/>
+                <Details className='sign-in-form__details' title='QuickLinks' links={links}/>
+            </form>
+    )
   }
 }
 

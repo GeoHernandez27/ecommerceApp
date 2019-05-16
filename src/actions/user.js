e4bba5b1a1c7e40f9dcd76089d9e85b0112333ae
GeoHernandez27 <leopardo2@gmail.com>
2019-05-16 12:59:11 -0600

    styled purchase detail

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,6 +1,14 @@
 import {
-    SET_USER_PURCHASES
+    SET_USER_PURCHASES,
+    SET_PURCHASE_DETAIL
 } from './types';
+
+export function setPurchaseDetail(_id) {
+    return ({
+        type: SET_PURCHASE_DETAIL,
+        payload: _id
+    })
+}
 
 export function fetchUserPurchases(){
     return ({
@@ -11,7 +19,7 @@ export function fetchUserPurchases(){
                 _id: 1,
                 total: 19.40,
                 orderNumber: 'A99435345346',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jon Smith',
@@ -23,7 +31,7 @@ export function fetchUserPurchases(){
                 _id: 2,
                 total: 14.89,
                 orderNumber: 'A99435234',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jon Smith',
@@ -35,7 +43,7 @@ export function fetchUserPurchases(){
                 _id: 3,
                 total: 10.99,
                 orderNumber: 'A994345345',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jon Smith',
@@ -47,7 +55,7 @@ export function fetchUserPurchases(){
                 _id: 4,
                 total: 19.40,
                 orderNumber: 'A99435677',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jon Smith',
@@ -59,7 +67,7 @@ export function fetchUserPurchases(){
                 _id: 5,
                 total: 34.99,
                 orderNumber: 'A9967867878',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jon Smith',
@@ -71,7 +79,7 @@ export function fetchUserPurchases(){
                 _id: 6,
                 total: 9.50,
                 orderNumber: 'A96786786',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jon Smith',
@@ -83,7 +91,7 @@ export function fetchUserPurchases(){
                 _id: 7,
                 total: 1.99,
                 orderNumber: 'A97867876456',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jon Smith',
@@ -95,7 +103,7 @@ export function fetchUserPurchases(){
                 _id: 8,
                 total: 11.50,
                 orderNumber: 'A78546354',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jon Smith',

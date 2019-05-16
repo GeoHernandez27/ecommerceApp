fe733fb09156d0ba2b21ab82aaf59c886790d41f
GeoHernandez27 <leopardo2@gmail.com>
2019-05-16 10:22:24 -0600

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -6,15 +6,102 @@ export function fetchUserPurchases(){
     return ({
         type: SET_USER_PURCHASES,
         payload: [
-            {
-                _id: 0,
-                title: 'purchase 1',
-                amount: 8.02
-            },
+            
             {
                 _id: 1,
-                title: 'purchase 2',
-                amount: 19.40
+                total: 19.40,
+                orderNumber: 'A99435345346',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jon Smith',
+                    shippingAddress: '1234 West State St'
+                }
+
+            },
+            {
+                _id: 2,
+                total: 14.89,
+                orderNumber: 'A99435234',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jon Smith',
+                    shippingAddress: '1234 West State St'
+                }
+
+            },
+            {
+                _id: 3,
+                total: 10.99,
+                orderNumber: 'A994345345',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jon Smith',
+                    shippingAddress: '1234 West State St'
+                }
+
+            },
+            {
+                _id: 4,
+                total: 19.40,
+                orderNumber: 'A99435677',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jon Smith',
+                    shippingAddress: '1234 West State St'
+                }
+
+            },
+            {
+                _id: 5,
+                total: 34.99,
+                orderNumber: 'A9967867878',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jon Smith',
+                    shippingAddress: '1234 West State St'
+                }
+
+            },
+            {
+                _id: 6,
+                total: 9.50,
+                orderNumber: 'A96786786',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jon Smith',
+                    shippingAddress: '1234 West State St'
+                }
+
+            },
+            {
+                _id: 7,
+                total: 1.99,
+                orderNumber: 'A97867876456',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jon Smith',
+                    shippingAddress: '1234 West State St'
+                }
+
+            },
+            {
+                _id: 8,
+                total: 11.50,
+                orderNumber: 'A78546354',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jon Smith',
+                    shippingAddress: '1234 West State St'
+                }
+
             }
         ]
     })

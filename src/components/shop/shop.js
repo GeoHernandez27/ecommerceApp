a7b1bf9b14bde4cbac791a1acbe6c1c9d8e3ae67
GeoHernandez27 <leopardo2@gmail.com>
2019-05-17 10:03:43 -0600

    developed shop reducer and categories

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -15,6 +15,7 @@ class Shop extends Component {
             }
         ]
         this.props.setHeaderLinks(headerLinks);
+        this.props.fetchShopCategories();
         //fetch shop products action
         //fetch navbar links
             //set navbar links

bc1608fed75d5b65824ac476ff5653a664b70f50
GeoHernandez27 <leopardo2@gmail.com>
2019-05-21 14:17:31 -0600

    built front of card and implented animation

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -4,15 +4,19 @@ import GreenPriceTag from "../greenPriceTag";
 
 class ShopProduct extends Component {
   render() {
-      const { __id, title, description, price } = this.props;
+    const { __id, title, description, price } = this.props;
     return (
       <div className="shop-product">
-        <div className="shop-product__title">{title}</div>
-        <div className="shop-product__description">{description}</div>
-        <GreenPriceTag className='shop-product__price' title={price} />
-        <Quantity className='shop-product__quantity'   quantity={1} />
-        <div className='shop-product__add-to-cart'>
-            Add to Cart
+        <div className='shop-product__front'>
+            <img className='shop-product__front_image' src='http://via.placeholder.com/220x220' />
+            <div className='shop-product__front__title'>{title} </div>
+        </div>
+        <div className="shop-product__back">
+          <div className="shop-product__back__title">{title}</div>
+          <div className="shop-product__back__description">{description}</div>
+          <GreenPriceTag className="shop-product__back__price" title={price} />
+          <Quantity className="shop-product__back__quantity" quantity={1} />
+          <div className="shop-product__back__add-to-cart">Add to Cart</div>
         </div>
       </div>
     );

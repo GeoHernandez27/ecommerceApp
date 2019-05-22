750e67f68f1f27d9e5b92d08e4dc2c36c3ac2083
GeoHernandez27 <leopardo2@gmail.com>
2019-05-22 16:32:16 -0600

    built review form details

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -3,7 +3,7 @@ import { reduxForm, Field } from "redux-form";
 
 import history from "../../history";
 
-import { FormButton } from '../formFields';
+import { FormButton } from "../formFields";
 
 import ReviewProducts from "./reviewProducts";
 
@@ -13,18 +13,17 @@ class ReviewForm extends Component {
 
     return (
       <form onSubmit={handleSubmit} className={`${className} review-form`}>
-        <div className='review-form__legend'>
-            <div className='review-form__legend__name'>Name</div>
-            <div className='review-form__legend__quantity'>Quantity</div>
-            <div className='review-form__legend__price'>Price</div>
-           
+        <div className="review-form__legend">
+          <div className="review-form__legend__name">Name</div>
+          <div className="review-form__legend__quantity">Quantity</div>
+          <div className="review-form__legend__price">Price</div>
         </div>
-        <ReviewProducts className='review-form__products'/>
+        <ReviewProducts className="review-form__products" />
 
         <div className="review-form__line" />
         <Field
           className="review-form__proceed"
-          onClick={() => history.push('/signin')}
+          onClick={() => history.push("/signin")}
           type="submit"
           title="Proceed to Checkout"
           name="proceed"
@@ -32,13 +31,31 @@ class ReviewForm extends Component {
         />
         <Field
           className="review-form__back"
-          onClick={() => history.push('/shop')}
+          onClick={() => history.push("/shop")}
           type="button"
           title="Back"
           name="back"
           short={true}
           component={FormButton}
         />
+        <div className="review-form__details review-details">
+          <div className="review-details__subtotal review-detail">
+            <div className="review-detail__title">Subtotal</div>
+            <div className="review-detail__price">$7.96</div>
+          </div>
+          <div className="review-details__tax review-detail">
+            <div className="review-detail__title">Tax</div>
+            <div className="review-detail__price">$1.25</div>
+          </div>
+          <div className="review-details__subtotal review-detail">
+            <div className="review-detail__title">Shipping</div>
+            <div className="review-detail__price">$0.00</div>
+          </div>
+          <div className="review-details__total review-detail review-detail-green">
+            <div className="review-detail__title review-detail-green__title">Total</div>
+            <div className="review-detail__price review-detail-green__price">$9.21</div>
+          </div>
+        </div>
       </form>
     );
   }

7acde335e929a50b6e37e14cb8125e1169bd4aac
GeoHernandez27 <leopardo2@gmail.com>
2019-05-23 13:48:21 -0600

    built shipping to component

--- src/components/information/paymentForm.js
+++ src/components/information/paymentForm.js
@@ -4,7 +4,8 @@ import { reduxForm, Field } from "redux-form";
 import { FormInput, FormButton } from "../formFields";
 
 import history from "../../history";
-import OrderSummary from './orderSummary';
+import OrderSummary from "./orderSummary";
+import { UnderlinedTitle } from "./infoHelp";
 
 class PaymentForm extends Component {
   render() {
@@ -28,7 +29,7 @@ class PaymentForm extends Component {
           name="card"
           component={FormInput}
         />
-                <Field
+        <Field
           className="payment-form__expiration"
           type="expiration"
           title="Expiration Date"
@@ -62,7 +63,17 @@ class PaymentForm extends Component {
           short={true}
           component={FormButton}
         />
-        <OrderSummary className='payment-form__order-summary' />
+        <OrderSummary className="payment-form__order-summary" />
+        <div className="payment-form__shipping-info shipping-info">
+          <UnderlinedTitle
+            className="shipping-info__title"
+            title="Shipping To"
+          />
+          <div className="shipping-info__name small-text">Jordan Hudgens</div>
+          <div className="shipping-info__address small-text">
+            1234 address goes here
+          </div>
+        </div>
       </form>
     );
   }
